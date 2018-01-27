69c5045d051b301d6cc9e98ea414d1d9b490f5a4
An Do <ando.headyrang@gmail.com>
2018-01-27 20:17:53 +0700

    Update with timer

--- app/scripts/app/taskController.js
+++ app/scripts/app/taskController.js
@@ -9,44 +9,104 @@
                 .icon('delete', '../assets/img/icons/ic_delete.svg', 48)
                 .icon('done', '../assets/img/icons/ic_done.svg', 48)
         })
-        .controller('TaskController', ['$scope', '$mdDialog', 'logger', TaskController]);
+        .controller('TaskController', ['$scope', '$interval', '$mdDialog', 'logger', TaskController]);
 
-    function TaskController($scope, $mdDialog, logger) {
-				$scope.parseInt = parseInt;
+    function TaskController($scope, $interval, $mdDialog, logger) {
+        // $scope
+        $scope.parseInt = parseInt;
 
-        var taskCtrl = this;
+        $scope.showAlert = function(inTitle, inContent) {
+            let alert = $mdDialog.alert({
+                title: inTitle,
+                textContent: inContent,
+                ok: "Got it!",
+            });
 
-        taskCtrl.onTextSelection = onTextSelection;
-        taskCtrl.onTaskEdittingBlur = onTaskEdittingBlur;
-				taskCtrl.editTask = editTask;
-				taskCtrl.toogleTask = toogleTask;
-        taskCtrl.doneEdittingTask = doneEdittingTask;
-        taskCtrl.deleteTask = deleteTask;
-				taskCtrl.deleteAllTasks = deleteAllTasks;
-				this.getTotalSpentMinute = getTotalSpentMinute;
+            $mdDialog
+                .show(alert)
+                .finally(function() {
+                    alert = undefined;
+                });
+        };
 
+        // $this
+        const appInteval = 1000;
+        const intervalInSecond = 1000 / appInteval;
+
+        let taskCtrl = this;
+
+        taskCtrl.runningTask = undefined;
         taskCtrl.tasks = [];
-        taskCtrl.addTask = addTask;
         taskCtrl.inputTask = "";
-        taskCtrl.spentMinute = 0;
+        taskCtrl.spentTime = 0;
 
-        function addTask() {
-            if (taskCtrl.inputTask) {
-                logger.info("Adding task: " + taskCtrl.inputTask);
-                taskCtrl.tasks.push({ name: taskCtrl.inputTask, isRunning: false, editing: false, spentMinute: 0 });
-                taskCtrl.inputTask = "";
+        let intervalFn;
+
+        function startTimer() {
+            if (angular.isDefined(intervalFn)) {
+                return;
+            }
+
+            intervalFn = $interval(function() {
+                let runningTask = taskCtrl.runningTask;
+                if (runningTask) {
+                    runningTask.spentTime += intervalInSecond;
+                    logger.info("Running task spent time: " + runningTask.spentTime);
+
+                    taskCtrl.spentTime = 0;
+                    taskCtrl.tasks.forEach(function(task) {
+                        taskCtrl.spentTime += task.spentTime;
+                    });
+                    logger.info("Total spent time: " + taskCtrl.spentTime);
+                }
+            }, appInteval);
+        }
+
+        function stopTimer() {
+            if (angular.isDefined(intervalFn)) {
+                $interval.cancel(intervalFn);
+                intervalFn = undefined;
             }
         }
 
+        taskCtrl.addTask = addTask;
+
+        function addTask() {
+            if (taskCtrl.inputTask) {
+                let taskName = taskCtrl.inputTask.trim();
+                if (taskName) {
+                    if (taskExists(taskName)) {
+                        $scope.showAlert("Alert", "Task already existed!");
+                    } else {
+                        logger.info("Adding task: " + taskName);
+                        taskCtrl.tasks.push({ name: taskName, isRunning: false, editing: false, spentTime: 0 });
+                        taskCtrl.inputTask = "";
+                    }
+                }
+            }
+        }
+
+        function taskExists(taskName) {
+            return taskCtrl.tasks.some(function(task) {
+                return task.name === taskName;
+            });
+        }
+
+        taskCtrl.onTextSelection = onTextSelection;
+
         function onTextSelection($event) {
             $event.target.select();
-        };
+        }
+
+        taskCtrl.onTaskEdittingBlur = onTaskEdittingBlur;
 
         function onTaskEdittingBlur(task) {
             angular.forEach(taskCtrl.tasks, function(key, value) {
                 key.editing = false;
             });
-        };
+        }
+
+        taskCtrl.editTask = editTask;
 
         function editTask(task) {
             angular.forEach(taskCtrl.tasks, function(key, value) {
@@ -54,18 +114,24 @@
             });
 
             task.editing = true;
-				};
-				
-				function toogleTask(task) {
-					let lazyIsRunning = task.isRunning;
-					angular.forEach(taskCtrl.tasks, function(key, value) {
-							key.isRunning = false;
-					});
+        }
 
-					task.isRunning = !lazyIsRunning;
-					logger.info("Toogle task: "+ task.name + ". isRunning: " + task.isRunning);
-					task.spentMinute += 13;
-				};
+        taskCtrl.toogleTask = toogleTask;
+
+        function toogleTask(task) {
+            let lazyIsRunning = task.isRunning;
+            angular.forEach(taskCtrl.tasks, function(key, value) {
+                key.isRunning = false;
+            });
+
+            task.isRunning = !lazyIsRunning;
+            logger.info("Toogle task: " + task.name + ". isRunning: " + task.isRunning);
+            if (task.isRunning) {
+                taskCtrl.runningTask = task;
+            }
+        }
+
+        taskCtrl.doneEdittingTask = doneEdittingTask;
 
         function doneEdittingTask(task) {
             if (!angular.element(task.srcElement)
@@ -74,28 +140,69 @@
                     key.editing = false;
                 });
             }
-        };
+        }
+
+        taskCtrl.deleteTask = deleteTask;
 
         function deleteTask(task) {
-					logger.info("Remove task: " + task.name);
-					let index = taskCtrl.tasks.indexOf(task);
-					taskCtrl.tasks.splice(index, 1);   
-        };
+            logger.info("Remove task: " + task.name);
+            let index = taskCtrl.tasks.indexOf(task);
+            taskCtrl.tasks.splice(index, 1);
+        }
+
+        taskCtrl.deleteAllTasks = deleteAllTasks;
 
         function deleteAllTasks() {
-						logger.info("Remove all tasks: ");
-						taskCtrl.tasks.length = 0;
-						taskCtrl.spentMinute = 0;
-				};
-				
-				function getTotalSpentMinute() {
-					let total = 0;
-					for (let i = 0; i < taskCtrl.tasks.length; i++){
-							let task = taskCtrl.tasks[i];
-							total += task.spentMinute;
-					}
-					return total;
-			};
+            logger.info("Remove all tasks: ");
+            taskCtrl.tasks.length = 0;
+            taskCtrl.spentTime = 0;
+        }
+
+        taskCtrl.stopRunningTasks = stopRunningTasks;
+
+        function stopRunningTasks() {
+            logger.info("Stop running tasks: ");
+            taskCtrl.tasks.forEach(function(task) {
+                if (task.isRunning) {
+                    task.isRunning = false;
+                }
+            });
+        }
+
+        taskCtrl.getSpentMinute = getSpentMinute;
+
+        function getSpentMinute(task) {
+            return parseInt((task.spentTime / 60) % 60);
+        }
+
+        taskCtrl.getSpentHour = getSpentHour;
+
+        function getSpentHour(task) {
+            return parseInt(task.spentTime / 3600);
+        }
+
+        taskCtrl.getTotalSpentMinute = getTotalSpentMinute;
+
+        function getTotalSpentMinute() {
+            return parseInt((taskCtrl.spentTime / 60) % 60);
+        }
+
+        taskCtrl.getTotalSpentHour = getTotalSpentHour;
+
+        function getTotalSpentHour() {
+            return parseInt(taskCtrl.spentTime / 3600);
+        }
+
+        function activate() {
+            startTimer();
+        }
+
+        function destroy() {
+            stopTimer();
+        }
+
+        // Register
+        activate();
     }
 
 })();
